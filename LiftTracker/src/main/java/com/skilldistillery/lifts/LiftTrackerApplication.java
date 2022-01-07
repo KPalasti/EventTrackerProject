569a1fdcc67f86cbd867506938cdfd1dced460bd
Kyle Palasti <Kyle.Palasti@gmail.com>
2022-01-07 15:40:38 -0700

    Create Spring Boot Project

---
+++ LiftTracker/src/main/java/com/skilldistillery/lifts/LiftTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.lifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftTrackerApplication.class, args);
+	}
+
+}
---
+++ LiftTracker/src/test/java/com/skilldistillery/lifts/LiftTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.lifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
